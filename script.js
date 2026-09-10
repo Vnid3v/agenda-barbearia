1edff1fb733399bc130cfd49c5f092df25f899c3
Vnid3v <vidamaceno99@hotmail.com>
2026-09-10 11:59:16 -0300

    Captura dados do agendamento

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ formulario.addEventListener("submit", function(evento) {
     evento.preventDefault();
     
     const nome = document.querySelector("#nome").value;
+    const servico = document.querySelector("#servico").value;
+    const data = document.querySelector("#data").value;
+    const horario = document.querySelector("#horario").value;
     
-    console.log(nome);
+    console.log("Nome:", nome);
+    console.log("Serviço:", servico);
+    console.log("Data:", data);
+    console.log("Horário:", horario);
 });
