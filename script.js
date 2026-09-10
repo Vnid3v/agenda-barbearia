17fe91ad40aadb62c4a0ff55b58622a2bc113cb0
Vnid3v <vidamaceno99@hotmail.com>
2026-09-10 11:49:00 -0300

    Captura envio do formulário

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Sistema da barbearia carregado!");
+
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    console.log("O formulário foi enviado!");
+});
